37e370ddcc8674213a89e24909b44d256b64a253
chandreshbhati-hammoq <chandresh.bhati@hammoq.com>
2026-08-17 15:39:33 +0530

    Fixed duplicated App export error

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,8 @@
 import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
+// import { Analytics } from "@vercel/analytics/react";
+// import { SpeedInsights } from "@vercel/speed-insights/react";
 
 
 export const getRouter = () => {
@@ -17,21 +17,3 @@ export const getRouter = () => {
 
   return router;
 };
-export function App() {
-  return (
-    <>
-      {/* Aapka baaki app code (Header, Outlet, Footer etc.) */}
-      <Analytics />
-    </>
-  );
-}
-
-export function App() {
-  return (
-    <>
-      {/* Aapka baaki app code */}
-      <Analytics />
-      <SpeedInsights />
-    </>
-  );
-}
